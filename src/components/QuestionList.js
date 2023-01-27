5d1494c93637b7b29d03a1b77a6e503433929c93
spectr-e <devs.josia@gmail.com>
2023-01-27 13:01:10 +0300

    imported QuestionCard component to QuestionList

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -5,19 +5,16 @@
 //   - removeQuestion: a function to remove a question
 
 import React from "react"
-
-
+import QuestionCard from "./QuestionCard"
 
 function QuestionList({ flashcards }) {
- return (
-   <div className="card-grid">
-     {flashcards.map((flashcard) => {
-       return <QuestionCard flashcard={flashcard} key={flashcard.id} />
-     })}
-   </div>
- )
-
+  return (
+    <div className="card-grid">
+      {flashcards.map((flashcard) => {
+        return <QuestionCard flashcard={flashcard} key={flashcard.id} />
+      })}
+    </div>
+  )
 }
-
 
 export default QuestionList
