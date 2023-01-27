92da645cfe5b98950736234f9baf261f80816ad1
Brian <wamwangimathenge@gmail.com>
2023-01-27 12:13:37 +0300

    Creating a Mobile Responsive Question Card

--- src/App.js
+++ src/App.js
@@ -1,87 +1,89 @@
-import React, { useState, useEffect, useRef } from 'react';
-import QuestionList from './components/QuestionList';
-import './App.css';
-import axios from 'axios';
+import React, { useEffect, useRef, useState } from "react"
+import { Switch, Route } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
 
+import NavBar from "./components/NavBar"
+import CardPage from "./containers/CardPage"
+import FormPage from "./containers/FormPage"
+import ListPage from "./containers/ListPage"
 
 function App() {
-
-  const [questioncards, setQuestioncards] = useState([])
+  const [flashcards, setFlashcards] = useState([])
   const [categories, setCategories] = useState([])
+  const categoryEl = useRef(null)
+  const amountEl = useRef(null)
 
-  const categoryEl = useRef()
-  const amountEl = useRef()
-
-
-  useEffect(() => {
-    axios
-      .get("https://opentdb.com/api_category.php")
-      .then(res => {
-        setCategories(res.data.trivia_categories)
-      })
-  }, [])
-
-
-  useEffect(() => {
-
-  }, [])
-
+  // A function that decodes the html elements in our questions
   function decodeString(str) {
-    const textArea = document.createElement('textarea')
+    const textArea = document.createElement("textarea")
     textArea.innerHTML = str
     return textArea.value
   }
 
+  // GET request to get all categories using fetch
+  useEffect(() => {
+    fetch("https://opentdb.com/api_category.php")
+      .then((response) => response.json())
+      .then((data) => {
+        setCategories(() => data.trivia_categories)
+      })
+      .catch((error) => console.error(error))
+  }, [])
 
   function handleSubmit(event) {
     event.preventDefault()
-    axios
-      .get("https://opentdb.com/api.php", {
-        params: {
+    // GET request to get all flashcards using fetch
+    fetch(
+      "https://opentdb.com/api.php?" +
+        new URLSearchParams({
           amount: amountEl.current.value,
-          category: categoryEl.current.value
-        }
-      })
-      .then(res => {
-        setQuestioncards(res.data.results.map((questionItem, index) => {
-          const answer = decodeString(questionItem.correct_answer)
-          const options = [...questionItem.incorrect_answers.map(a => decodeString(a)), answer]
-          return {
-            id: `${index}-${Date.now()}`,
-            question: decodeString(questionItem.question),
-            answer: questionItem.correct_answer,
-            options: options.sort(() => Math.random() - .5)
-          }
-        }))
+          category: categoryEl.current.value,
+        })
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setFlashcards(
+          data.results.map((questionItem, index) => {
+            // Take the correct answer and add it to array of incorrect answers and randomly sort them so that the correct answer doesn't appear first
+            const answer = decodeString(questionItem.correct_answer)
+            const options = [
+              ...questionItem.incorrect_answers.map((ans) => decodeString(ans)),
+              answer,
+            ]
+            return {
+              id: `${index}-${Date.now()}`,
+              question: decodeString(questionItem.question),
+              answer: questionItem.correct_answer,
+              options: options.sort(() => Math.random() - 0.5),
+            }
+          })
+        )
       })
   }
 
   return (
-    <>
-    <form className="header" onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label htmlFor="category">Category</label>
-        <select id="category" ref={categoryEl}>
-          {categories.map(category => {
-            return <option value={category.id} key={category.id}>{category.name}</option>
-          })}
-        </select>
-      </div>
-      <label htmlFor="amount">No of Questions</label>
-      <input type="number" id="amount" min="1" step="1" defaultValue={10} ref={amountEl} />
-      <div className="form-group">
-        <button className="btn">Generate</button>
-
-      </div>
-    </form>
-      <div className="container">
-      <QuestionList questioncards={questioncards} />
-      </div>
-    </>
-    
-    
-  );
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <CardPage
+            flashcards={flashcards}
+            categories={categories}
+            categoryEl={categoryEl}
+            amountEl={amountEl}
+            handleSubmit={handleSubmit}
+          />
+        </Route>
+        <Route path="/create">
+          <FormPage />
+        </Route>
+        <Route path="/questions">
+          <ListPage flashcards={flashcards} />
+        </Route>
+      </Switch>
+    </div>
+  )
 }
 
-
-export default App;
+export default App
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import QuestionCard from './QuestionCard'
+// Lists all the questions and answers in our database and ones added by the user.
+// Props to be passed:
+//   - questions: an array of questions
+//   - answers: an array of answers
+//   - removeQuestion: a function to remove a question
 
-export default function QuestionList({questioncards}) {
-  return (
-    <div className="card-grid">
-      {questioncards.map(questioncard => {
-        return <QuestionCard questioncard={questioncard} key={questioncard.id} />
-      })}
-    </div>
-  )
+import React from "react"
+
+function QuestionList() {
+  return {}
 }
+
+export default QuestionList
