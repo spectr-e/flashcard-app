8c7fc11143bd3b389c4580bbebce31451a1a5fb7
spectr-e <devs.josia@gmail.com>
2023-01-27 10:00:39 +0300

    navigation bar frontend

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,9 +5,35 @@
 // - Questions : This page will render our QuestionList.js component.
 
 import React from "react"
+import { NavLink } from "react-router-dom"
+import logo from "../images/logo.png"
 
 function NavBar() {
-  return <div>NavBar</div>
+  return (
+    <div className="navbar">
+      <div className="navbar-content">
+        <div className="navbar-brand">
+          <NavLink to="/" exact className="navbar-item">
+            <img src={logo} alt="logo"></img>
+          </NavLink>
+          <span className="nav-text">Uwezo FlashCard App</span>
+        </div>
+        <div id="navMenu" className="navbar-menu">
+          <div className="navbar-start has-text-centered">
+            <NavLink to="/" exact className="navbar-item">
+              Home
+            </NavLink>
+            <NavLink to="/create-quiz" exact className="navbar-item">
+              Create
+            </NavLink>
+            <NavLink to="/questions" exact className="navbar-item">
+              Questions
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default NavBar
